e1a6ebe8e825d861584deab4e148c85d5dbb7e62
Nathan Villaescusa <nathan@signed0.com>
2018-05-05 18:15:51 -0700

    Use better HN API

--- hn_jobs.py
+++ hn_jobs.py
@@ -1,49 +1,35 @@
 #!/usr/bin/env python3
 
-import os
 import logging
-import sys
-
-import requests
-from flask import Flask, render_template
 
 from bs4 import BeautifulSoup
+from flask import Flask, render_template, redirect
+import requests
+
+API_BASE = 'https://hn.algolia.com/api/v1'
 
 PLACE_MATCHES = ('SF', 'San Francisco', 'san francisco')
 BODY_MATCHES = ('java', 'python')
-FETCH_PAGES = True
+
 
 app = Flask(__name__)
 
 
-def get_hn_comments(article_id):
-    def _page_comments(page=1):
-        logging.info("Fetching page %d for %d", page, article_id)
-        url = 'https://news.ycombinator.com/item?id=%d&p=%d' % (article_id, page)
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'html.parser')
-        comments = soup.find_all('div', class_='comment')
-        comments = (c.find('span', class_='c00') for c in comments)
-        def clean_comment_div(c):
-            strings = list(c.strings)[::-1]
-            first_string = next(i for i, x in enumerate(strings) if x.strip() not in ('', 'reply'))
-            return '\n'.join(reversed(strings[first_string:]))
-        comments = (clean_comment_div(c) for c in comments if c)
-        comments = (c for c in comments if '|' in c)
+def get_latest_hiring_story():
+    url = API_BASE + '/search_by_date?query=Ask+HN+Who+is+hiring&tags=author_whoishiring,ask_hn'
+    r = requests.get(url)
+    return int(r.json()['hits'][0]['objectID'])
 
-        is_last_page = soup.find('a', class_="morelink") is None
 
-        return comments, is_last_page
+def get_hn_comments(story_id):
+    r = requests.get('%s/items/%d' % (API_BASE, story_id))
+    return [c for c in r.json()['children'] if c['text']]
 
-    for page in range(1, 25):
-        comments, is_last_page = _page_comments(page)
-        yield from comments
-        if not FETCH_PAGES or is_last_page:
-            break
 
-def get_jobs(article_id):
-    for comment in get_hn_comments(article_id):
-        head, *body = comment.split('\n')
+def get_jobs(story_id):
+    for comment in get_hn_comments(story_id):
+        soup = BeautifulSoup(comment['text'], 'html.parser')
+        head, *body = '\n'.join(soup.strings).split('\n')
         head_parts = [p for p in (p.strip() for p in head.split('|')) if p]
         yield {
             'head': head,
@@ -51,6 +37,7 @@ def get_jobs(article_id):
             'parts': head_parts[1:],
             'body': '<br>'.join(body)
             }
+
 
 def filter(job):
     if not any(p in job['head'] for p in PLACE_MATCHES):
@@ -60,9 +47,16 @@ def filter(job):
         return False
     return True
 
-@app.route("/hn/<int:article_id>")
-def main(article_id):
-    jobs = list(get_jobs(article_id))
+
+@app.route("/hn")
+def latest_jobs():
+    story_id = get_latest_hiring_story()
+    return redirect("/hn/%d" % story_id, code=302)
+
+
+@app.route("/hn/<int:story_id>")
+def monthly_jobs(story_id):
+    jobs = list(get_jobs(story_id))
     filtered_jobs = [job for job in jobs if filter(job)]
     logging.info("%d total jobs, %d after filters", len(jobs), len(filtered_jobs))
     return render_template('results.html', jobs=filtered_jobs)
@@ -71,4 +65,3 @@ def main(article_id):
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     app.run(debug=True)
-
